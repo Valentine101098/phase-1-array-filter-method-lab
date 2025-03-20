f71bca4eb4011ccf7678a2efe4bb85dc42f9aef3
Valentine Wanjiru <kt116tabitha@gmail.com>
2025-03-20 16:39:32 +0300

    uses the array filter method

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+//Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+}
+
+
+function fuzzyMatch(drivers, name) {
+    const lowerName = name.toLowerCase();
+    return drivers.filter(driver => {
+      const lowerDriver = driver.toLowerCase();
+      return lowerDriver.startsWith(lowerName);
+    });
+  }
+
+  function matchName(drivers, name) {
+      return drivers.filter(driver => driver.name === name);
+  }
